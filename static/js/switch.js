c34f914a381b42dd2318e2b43794227e96a8a3ee
Paul <paulvdb08@hotmail.com>
2018-01-02 19:11:15 +0100

    Het veranderen van het "checked" attribuut na het klikken op de switch werkt!

--- static/js/switch.js
+++ static/js/switch.js
@@ -3,16 +3,40 @@ document.addEventListener('DOMContentLoaded', function () {
     var switches = document.getElementsByClassName('switch');
 
     for (var i = 0; i < switches.length; i++) {
-        switches[i].addEventListener('click', printMessage);
+        switches[i].addEventListener('click', switchClicked);
     }
 
-    function printMessage() {
-        console.log("switch aangeklikt!");
+    function switchClicked() {
+        var attr = $(this).attr('checked');
+
+        if (typeof attr !== typeof undefined && attr !== false) {
+            $(this).removeAttr('checked');
+            turnLightOff(this);
+        }
+        else {
+            $(this).attr('checked', 'checked');
+            turnLightOn(this)
+        }
+    }
+
+    function turnLightOn(lamp) {
+        console.log("Turn light on");
 
         $.ajax({
             type: "POST",
             url: '/ajax/switch_light/',
-            data: {'light_ip': this.id},
+            data: {'light_ip': lamp.id},
+            dataType: 'json'
+        });
+    }
+
+    function turnLightOff(lamp) {
+        console.log("Turn light off");
+
+        $.ajax({
+            type: "POST",
+            url: '/ajax/switch_light/',
+            data: {'light_ip': lamp.id},
             dataType: 'json'
         });
     }
